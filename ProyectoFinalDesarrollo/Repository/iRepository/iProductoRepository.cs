b7a24d683e9f2afe43c614a995d3a0294b18759a
gsolorzanoc <gsolorzanoc@miumg.edu.gt>
2020-11-02 23:41:40 -0600

    Integracion de cambios del modulo de productos

---
+++ ProyectoFinalDesarrollo/Repository/iRepository/iProductoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ProyectoFinalDesarrollo.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Web.UI.WebControls.WebParts;
+
+namespace ProyectoFinalDesarrollo.Repository.iRepository
+{
+    public interface iProductoRepository
+    {
+        ICollection<ProductosModel> GetProductoModels();
+        ProductosModel GetProducto(int CodigoProducto);
+        bool CreaProducto(ProductosModel producto);
+        bool UpdateProducto(ProductosModel producto);
+        bool DeleteProducto(ProductosModel producto);
+        bool GuardarProducto();
+    }
+}

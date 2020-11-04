1b46a526a4535527a9c057a9e3c8f38b2323e25b
MAYARI <cmayarilizbeth@hotmail.com>
2020-11-03 19:02:35 -0600

    integracion de cambion

---
+++ ProyectoFinalDesarrollo/Mappers/ClienteMappers.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProyectoFinalDesarrollo.Models;
+using ProyectoFinalDesarrollo.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalDesarrollo.Mappers
+{
+    public class ClienteMappers:Profile
+    {
+        public ClienteMappers()
+        {
+            //hacer una relacion entre DTO y modelos iniciales
+            CreateMap<ClientesModel, ClienteModelDTO>().ReverseMap();
+            
+        }
+    }
+}
